80bdddfa7c25cb99eb5ab22f5d0aaf4062836dee
anantraj00429 <161113121+anantraj00429@users.noreply.github.com>
2024-03-01 22:58:03 -0800

    Add files via upload
    change

---
+++ main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var img = document.querySelector(".image5");
+img.style.height = "91vh";
+img.style.left = "23%";
+img.style.top = "-42px";
+
+var choco = document.querySelector("#choco");
+choco.style.top = "-370px";
+choco.style.left = "-8px";
+
+var Dark = document.querySelector("#Dark");
+Dark.style.left = "3.5%";
